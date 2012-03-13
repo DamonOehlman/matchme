9ee8304a3a36418773b6b196a0e362275166bfb2
Damon Oehlman <damon.oehlman@sidelab.com>
2012-03-13 18:04:57 +1000

    Removed the need for the matchme redirect in the node_modules folder

--- test/lte.js
+++ test/lte.js
@@ -1,5 +1,5 @@
 describe('<= tests', function() {
-    var matchme = require('matchme'),
+    var matchme = require('../matchme'),
         expect = require('expect.js'),
         testdata = require('./helpers/testdata');
 
