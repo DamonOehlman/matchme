db2fd29a1d87dc77631b0f2c661b6638e048236e
Damon Oehlman <damon.oehlman@gmail.com>
2013-08-05 21:35:58 +1000

    Maintenance tweaks

---
+++ test/all.js
@@ -0,0 +1,13 @@
+require('./chaining');
+require('./complex');
+require('./equality');
+require('./filters');
+require('./gt');
+require('./gte');
+require('./inequality');
+require('./logicalprop');
+require('./lt');
+require('./lte');
+require('./nested');
+require('./regex');
+require('./safety');
