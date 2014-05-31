4b18590ba7e7a54f88f44ac9e15e2b45390a5c7d
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-31 18:24:21 +1000

    Added case insensitive inquality match test

--- test/inequality.js
+++ test/inequality.js
@@ -3,7 +3,10 @@ var matchme = require('../'),
     testdata = require('./helpers/testdata');
 
 test('!= tests', function(t) {
-    t.plan(2);
-    t.ok(matchme(testdata.fred, 'name != Bob'));
-    t.notOk(matchme(testdata.fred, 'name != Fred'));
+  t.plan(4);
+  t.ok(matchme(testdata.fred, 'name != Bob'));
+  t.notOk(matchme(testdata.fred, 'name != Fred'));
+
+  t.ok(matchme(testdata.fred, 'name != fred'));
+  t.notOk(matchme(testdata.fred, 'name !~ fred'));
 });
