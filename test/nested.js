acaa6391fc97e2c94b83a09254bba892e8fe566c
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-31 17:30:01 +1000

    Added quoted strings to test cases

--- test/nested.js
+++ test/nested.js
@@ -7,3 +7,13 @@ test('nested property access', function(t) {
     t.ok(matchme(testdata.location1, 'area.name == Brisbane'));
     t.notOk(matchme(testdata.location1, 'area.size > 500'), 'deals with properties that dont exist gracefully');
 });
+
+test('nested property access (quoted)', function(t) {
+    t.plan(1);
+    t.ok(matchme(testdata.location1, 'area.name == "Brisbane"'));
+});
+
+test('nested property access (single quoted)', function(t) {
+    t.plan(1);
+    t.ok(matchme(testdata.location1, 'area.name == \'Brisbane\''));
+});
