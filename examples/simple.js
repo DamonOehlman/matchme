59e5f59c1364b3ee0a2b864db91ca16bf3ec5af6
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-31 18:04:52 +1000

    Update examples

--- examples/simple.js
+++ examples/simple.js
@@ -1,14 +1,17 @@
 var matchme = require('..');
 var test = { name: 'Ted', age: 40 };
 
+console.log(matchme(test, 'name == Ted'));
+// --> true
+
 console.log(matchme(test, 'name == ted'));
+// --> false
+
+console.log(matchme(test, 'name ~= ted'));
 // --> true
 
 console.log(matchme(test, 'age > 25'));
 // --> true
 
-console.log(matchme(test, { caseSensitive: true }, 'name == ted'));
-// --> false
-
-console.log(matchme(test, 'name == ted && age > 45'));
+console.log(matchme(test, 'name ~= ted && age > 45'));
 // --> false
